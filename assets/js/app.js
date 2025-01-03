42a752ea6872e79bee6cd8ff942755f96b3d0ef7
Aravind Terdal <aravindterdal@gmail.com>
2025-01-03 16:23:06 +0530

    finial update

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,15 +4,19 @@ const tabHeading = [...document.querySelectorAll('.tabHeading li')];
 const tabPanel = [...document.querySelectorAll('.tabPanel')]
 
 const onTabHeadClick = (eve) => {
-    cl(eve.target)
-    cl(eve.target.getAttribute('data-id'))
-    let getId = eve.target.getAttribute('data-id')
+    // cl(eve.target)
+    // cl(eve.target.getAttribute('data-id'))
+    let getId = eve.target.getAttribute('data-id');
 
     tabPanel.forEach(tabpan => {
         tabpan.classList.remove('active')
     })
 
-    document.getElementById(getId).classList.add('active')
+    document.getElementById(getId).classList.add('active');
+
+    tabHeading.forEach(li => li.classList.remove('active'));
+
+    eve.target.classList.add('active');
 }
 
 
